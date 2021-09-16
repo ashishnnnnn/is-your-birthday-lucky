7d9b3cf3a0ad7ca3e1234293a253959d4aa6216e
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2021-09-16 12:45:50 +0530

    feat - accounting the negative values as lucky number

--- script.js
+++ script.js
@@ -3,25 +3,32 @@ var number = document.querySelector("#lucky-number");
 var button = document.querySelector(".check");
 var message = document.querySelector(".message");
 
-button.addEventListener("click",function verify_dob(){
-    var sum = sum_of_dates(dob.value);
-    var rem = sum%Number(number.value);
-    if(rem==0){
-        message.innerText = "Your BirthDay Is Lucky!!🥳🥳";
+button.addEventListener("click", function verify_dob() {
+  var lucky_number = Number(number.value);
+  message.innerText = "";
+  var sum = sum_of_dates(dob.value);
+  if (lucky_number <= 0) {
+    message.style.color = "red";
+    message.innerText = "Please Enter Positive value as Lucky Number";
+  } else {
+    var rem = sum % lucky_number;
+    if (rem == 0) {
+      message.style.color = "green";
+      message.innerText = "Your BirthDay Is Lucky!!🥳🥳";
+    } else {
+      message.style.color = "red";
+      message.innerText = "Sorry Your BirthDay is Not Lucky😔😔";
     }
-    else{
-        message.innerText = "Sorry Your BirthDay is Not Lucky😔😔";
-    }
-})
+  }
+});
 
-
-function sum_of_dates(dob){
-    var sum = 0;
-    for(var i=0;i<dob.length;i++){
-        var k = dob[i];
-        if(k>='0' && k<='9'){
-            sum+=Number(k);
-        }
+function sum_of_dates(dob) {
+  var sum = 0;
+  for (var i = 0; i < dob.length; i++) {
+    var k = dob[i];
+    if (k >= "0" && k <= "9") {
+      sum += Number(k);
     }
-    return sum;
+  }
+  return sum;
 }
